ad0cb721dc9ddc19923132402c585e7e56524862
Saku Arho <sakurho@gmail.com>
2024-09-16 20:58:25 +0300

    Add the spotlight component to the Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
