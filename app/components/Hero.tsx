737b00fa0af85418dcdf872882c81669c7c1bdc4
Saku Arho <sakurho@gmail.com>
2024-09-16 20:59:46 +0300

    Add some css to the spotlight.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,10 @@ const Hero = () => {
   return (
     <div className="pb-20 pt-36">
       <div>
-        <Spotlight />
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
       </div>
     </div>
   );
