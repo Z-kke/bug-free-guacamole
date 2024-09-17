a804bcbb148b422420af801f83cdaa5202f9449f
Saku Arho <sakurho@gmail.com>
2024-09-17 14:34:55 +0300

    Add BentoGrid into Grid component.

---
+++ components/ui/Grid.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+const Grid = () => {
+  return (
+    <section>
+      <BentoGrid>Grid</BentoGrid>{" "}
+    </section>
+  );
+};
+
+export default Grid;
