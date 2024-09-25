ce9bf548b01bbc15d504d2270cbed8a9c82eef96
Saku Arho <sakurho@gmail.com>
2024-09-25 18:12:17 +0300

    Fix bento grid not being aligned correctly

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -3,7 +3,7 @@ import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 import { gridItems } from "@/data";
 const Grid = () => {
   return (
-    <section>
+    <section id="about">
       <BentoGrid>
         {gridItems.map((item) => (
           <BentoGridItem
@@ -11,9 +11,10 @@ const Grid = () => {
             key={item.id}
             title={item.title}
             description={item.description}
+            className={item.className}
           />
         ))}
-      </BentoGrid>{" "}
+      </BentoGrid>
     </section>
   );
 };
