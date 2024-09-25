52a428af5a3589421258b49612aa727085256550
Saku Arho <sakurho@gmail.com>
2024-09-25 18:16:25 +0300

    Destructure the rest of the props as well.

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,16 +5,27 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {gridItems.map(({ id, title, description, className, img }) => (
-          <BentoGridItem
-            id={id}
-            key={id}
-            title={title}
-            description={description}
-            className={className}
-            img={img}
-          />
-        ))}
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+            />
+          ),
+        )}
       </BentoGrid>
     </section>
   );
