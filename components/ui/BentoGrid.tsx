b009fb30826061982c602af145c281c74b18da9d
Saku Arho <sakurho@gmail.com>
2024-09-25 19:35:50 +0300

    Fix the import

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import { GlobeDemo } from "./Globe";
+import GridGlobe from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -87,7 +87,7 @@ export const BentoGridItem = ({
             {title}
           </div>
         </div>
-        {id === 2 && <GlobeDemo />}
+        {id === 2 && <GridGlobe />}
       </div>
     </div>
   );
