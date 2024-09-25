b3302f94fe6db8e3c63b29c7ffbd9fbd7454ccf9
Saku Arho <sakurho@gmail.com>
2024-09-25 18:19:01 +0300

    Realize that the last resolution was stupid.

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,30 +5,9 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {gridItems.map(
-          ({
-            id,
-            title,
-            description,
-            className,
-            img,
-            imgClassName,
-            titleClassName,
-            spareImg,
-          }) => (
-            <BentoGridItem
-              id={id}
-              key={id}
-              title={title}
-              description={description}
-              className={className}
-              img={img}
-              imgClassName={imgClassName}
-              titleClassName={titleClassName}
-              spareImg={spareImg}
-            />
-          ),
-        )}
+        {gridItems.map((item) => (
+          <BentoGridItem key={item.id} {...item} />
+        ))}
       </BentoGrid>
     </section>
   );
