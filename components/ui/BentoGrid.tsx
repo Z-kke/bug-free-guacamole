849a5468a289a262febdf63203126bd6da20f312
Saku Arho <sakurho@gmail.com>
2024-09-30 16:41:17 +0300

    Add the copy function and also fix some of the css.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -55,8 +55,18 @@ export const BentoGridItem = ({
     setCopied(true);
   };
 
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   const rightStack = ["GraphQL", "DynamoDB", "NodeJS", "Express"];
   const leftStack = ["React.js", "VueJS", "Next.js", "Typescript"];
+
   return (
     <div
       className={cn(
@@ -65,42 +75,34 @@ export const BentoGridItem = ({
       )}
       style={{ background: "rgb(4,7,29)", backgroundColor: "gradient..." }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full`}>
-        <div className="w-full h-full absolute">
-          {img && (
-            <img
-              src={img}
-              alt={img}
-              className={cn(imgClassName, "object-cover, object-center")}
-            />
-          )}
-        </div>
-        <div
-          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}
-        >
-          {spareImg && (
-            <img
-              src={spareImg}
-              alt={spareImg}
-              className={"object-cover, object-center w-full h-full"}
-            />
-          )}
-        </div>
+      <div
+        className={cn(
+          "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+          className,
+        )}
+        style={{
+          background: "rgb(4,7,29)",
+          backgroundColor:
+            "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+        }}
+      >
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
         )}
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:tranlate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10",
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10",
           )}
         >
-          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+          <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
             {title}
           </div>
           {id === 2 && <GridGlobe />}
@@ -132,27 +134,25 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
-          <div className="mt-5 relative">
-            <div className="absolute -bottom-5 right-0">
-              <Lottie
-                options={{
-                  loop: copied,
-                  autoplay: copied,
-                  animationData,
-                  rendererSettings: {
-                    preserveAspectRatio: "xMidYmid slice",
-                  },
-                }}
-              />
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
+              >
+                <Lottie options={defaultOptions} height={200} width={400} />
+              </div>
+
               <MagicButton
-                title={copied ? "Email copied" : "Copy my email"}
-                icon=<IoCopyOutline />
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
                 position="left"
-                otherClasses="!bg-[#161a32]"
                 handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
               />
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
